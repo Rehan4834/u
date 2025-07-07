57fb6b14c96698be12a941af0da7032d01e94b19
Rehan4834 <rehanjjamadar786@gmail.com>
2025-07-07 11:48:55 +0530

    Add files via upload

---
+++ Bike.java
@@ -0,0 +1,11 @@
+class Bike
+{
+    Bike()
+    {
+        System.out.println("Bike is not created");
+    }
+    public static void main (String args[])
+    {
+        Bike b= new Bike();
+    }
+}
